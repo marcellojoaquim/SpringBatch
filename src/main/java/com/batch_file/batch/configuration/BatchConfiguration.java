72096aa4a01ca874851c8238bbd977459a1168ef
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2025-05-08 15:20:57 -0300

    implementado batch coniguration

--- src/main/java/com/batch_file/batch/configuration/BatchConfiguration.java
+++ src/main/java/com/batch_file/batch/configuration/BatchConfiguration.java
@@ -1,7 +1,13 @@
 package com.batch_file.batch.configuration;
 
+import com.batch_file.batch.listener.JobCompletionNotificationListener;
 import com.batch_file.batch.model.Person;
 import com.batch_file.batch.processor.PersonItemProcessor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
@@ -9,6 +15,7 @@ import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -38,6 +45,28 @@ public class BatchConfiguration {
                 .sql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)")
                 .dataSource(dataSource)
                 .beanMapped()
+                .build();
+    }
+
+    @Bean
+    public Job importUserJob(JobRepository jobRepository, Step step1, JobCompletionNotificationListener listener) {
+        return new JobBuilder("importUserJob", jobRepository)
+                .listener(listener)
+                .start(step1)
+                .build();
+    }
+
+    @Bean
+    public  Step step1(JobRepository jobRepository,
+                       DataSourceTransactionManager transactionManager,
+                       FlatFileItemReader<Person> reader,
+                       PersonItemProcessor processor,
+                       JdbcBatchItemWriter<Person> writer) {
+        return new StepBuilder("step1", jobRepository).
+                <Person, Person>chunk(3, transactionManager)
+                .reader(reader)
+                .processor(processor)
+                .writer(writer)
                 .build();
     }
 }
---
+++ src/main/java/com/batch_file/batch/listener/JobCompletionNotificationListener.java
@@ -0,0 +1,4 @@
+package com.batch_file.batch.listener;
+
+public class JobCompletionNotificationListener{
+}
