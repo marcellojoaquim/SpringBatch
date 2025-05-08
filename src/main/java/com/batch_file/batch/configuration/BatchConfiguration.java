04f74b09d321d66ba9f8d495e552184ee907ba87
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2025-05-08 15:07:16 -0300

    Add file reader and batch writer configuration

---
+++ src/main/java/com/batch_file/batch/configuration/BatchConfiguration.java
@@ -0,0 +1,43 @@
+package com.batch_file.batch.configuration;
+
+import com.batch_file.batch.model.Person;
+import com.batch_file.batch.processor.PersonItemProcessor;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class BatchConfiguration {
+
+    @Bean
+    public FlatFileItemReader<Person> reader() {
+        return new FlatFileItemReaderBuilder<Person>()
+                .name("PersonItemReader")
+                .resource(new ClassPathResource("sample-data.csv"))
+                .delimited()
+                .names("firstName", "lastName")
+                .targetType(Person.class)
+                .build();
+    }
+
+
+    @Bean
+    public PersonItemProcessor processor() {
+        return new PersonItemProcessor();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Person> writer(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Person>()
+                .sql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)")
+                .dataSource(dataSource)
+                .beanMapped()
+                .build();
+    }
+}
